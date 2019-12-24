a8a6bbe0ee42383421af31477bf4b38b66dd8c3b
hsimao <e087754958@gmail.com>
2019-12-24 21:09:45 +0800

    init

---
+++ main.js
@@ -0,0 +1,72 @@
+// Modules
+const { app, BrowserWindow } = require('electron')
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow
+
+// Create a new BrowserWindow when `app` is ready
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1000,
+    height: 800,
+    webPreferences: { nodeIntegration: true },
+    // 預設背景顏色
+    backgroundColor: '#2c92f9',
+    // show: false
+  })
+
+  // Load index.html into the new BrowserWindow
+  mainWindow.loadFile('index.html')
+  // mainWindow.loadFile('https://www.google.com/')
+
+  // 監聽視窗
+  // mainWindow.on('ready-to-show', mainWindow.show)
+
+  // Open DevTools - Remove for PRODUCTION!
+  mainWindow.webContents.openDevTools()
+
+  // Listen for window being closed
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  })
+}
+
+// Electron `app` is ready
+app.on('ready', () => {
+  console.log('is Ready')
+
+  // 取得 desktop 路徑
+  console.log(app.getPath('desktop'))
+  console.log(app.getPath('music'))
+  console.log(app.getPath('temp'))
+  // 當前應用程式路徑
+  console.log(app.getPath('userData'))
+
+  createWindow()
+})
+
+// Quit when all windows are closed - (Not macOS - Darwin)
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+// 關閉應用程式事件監聽
+app.on('before-quit', e => {
+  console.log('before-quit')
+})
+
+// 應用程式 blur
+app.on('browser-window-blur', e => {
+  console.log('App 取消焦點')
+})
+
+// 應用程式 focus
+app.on('browser-window-focus', e => {
+  console.log('App focus')
+})
+
+// When app icon is clicked and app is running, (macOS) recreate the BrowserWindow
+app.on('activate', () => {
+  if (mainWindow === null) createWindow()
+})
