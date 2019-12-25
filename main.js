786158c619c6c5f428362509b162b0c45664ee0d
hsimao <e087754958@gmail.com>
2019-12-25 21:28:44 +0800

    add: browser api 相關屬性、事件

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const { app, BrowserWindow } = require('electron')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let mainWindow, secWindow
 
 // Create a new BrowserWindow when `app` is ready
 function createWindow() {
@@ -11,34 +11,54 @@ function createWindow() {
     width: 1000,
     height: 800,
     webPreferences: { nodeIntegration: true },
-    backgroundColor: '#2c92f9'
+    backgroundColor: '#2c92f9',
+  })
+
+  secWindow = new BrowserWindow({
+    width: 700,
+    height: 600,
+    resizable: false, // 禁止縮放
+    webPreferences: { nodeIntegration: true },
+    backgroundColor: '#2c92f9',
   })
 
   // Load index.html into the new BrowserWindow
   mainWindow.loadFile('index.html')
+  secWindow.loadFile('index.html')
 
   // Open DevTools - Remove for PRODUCTION!
-  mainWindow.webContents.openDevTools()
+  // mainWindow.webContents.openDevTools()
 
   // Listen for window being closed
   mainWindow.on('closed', () => {
     mainWindow = null
   })
+  secWindow.on('closed', () => {
+    // 當關閉 secWindow 時將 mainWindow 視窗放到最大
+    mainWindow.maximize()
+    // secWindow = null
+  })
+
+  // 監聽 focus
+  mainWindow.on('focus', () => {
+    console.log('mainWindow focus')
+  })
+  secWindow.on('focus', () => {
+    console.log('secWindow focus')
+  })
+
+  // 取得 mainWindow id
+  console.log('mainWindow ID:', mainWindow.id)
+
+  // 取得指定 window id 資料
+  console.log(BrowserWindow.fromId(mainWindow.id))
+
+  // 取得所有 window 資料
+  console.log(BrowserWindow.getAllWindows())
 }
 
 // Electron `app` is ready
-app.on('ready', () => {
-  console.log('is Ready')
-
-  // 取得 desktop 路徑
-  console.log(app.getPath('desktop'))
-  console.log(app.getPath('music'))
-  console.log(app.getPath('temp'))
-  // 當前應用程式路徑
-  console.log(app.getPath('userData'))
-
-  createWindow()
-})
+app.on('ready', createWindow)
 
 // Quit when all windows are closed - (Not macOS - Darwin)
 app.on('window-all-closed', () => {
@@ -48,16 +68,6 @@ app.on('window-all-closed', () => {
 // 關閉應用程式事件監聽
 app.on('before-quit', e => {
   console.log('before-quit')
-})
-
-// 應用程式 blur
-app.on('browser-window-blur', e => {
-  console.log('App 取消焦點')
-})
-
-// 應用程式 focus
-app.on('browser-window-focus', e => {
-  console.log('App focus')
 })
 
 // When app icon is clicked and app is running, (macOS) recreate the BrowserWindow
