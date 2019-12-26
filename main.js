520cc9bace360c7aacb32f8cbc3cb4defef2b201
hsimao <e087754958@gmail.com>
2019-12-26 21:56:42 +0800

    add: session cookies

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Modules
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, session } = require('electron')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -7,6 +7,21 @@ let mainWindow
 
 // Create a new BrowserWindow when `app` is ready
 function createWindow() {
+  let ses = session.defaultSession
+
+  // 取得 session 內的 cookie 資料, 第一個參數空物件表示要取得所有資料
+  // ses.cookies.get({}, (err, cookies) => {
+  //   console.log('cookies', cookies)
+  // })
+
+  // 將取得 cookies 封裝成 function
+  const getCookies = name => {
+    const target = name ? { name: name } : {}
+    ses.cookies.get(target, (err, cookies) => {
+      console.log('cookies', cookies)
+    })
+  }
+
   mainWindow = new BrowserWindow({
     width: 1000,
     height: 800,
@@ -16,6 +31,27 @@ function createWindow() {
 
   // Load index.html into the new BrowserWindow
   mainWindow.loadFile('index.html')
+
+  // 載入 github 頁面
+  // main
+
+  // 刪除 cookie
+  ses.cookies.remove('https://github.com/hsimao/', 'cookie1', err => {
+    getCookies('cookie1')
+  })
+  // 設置 cookie 到瀏覽器上
+  let cookie = { name: 'cookie1', value: 'electron', url: 'https://github.com/hsimao/', expirationDate: 1613852855 }
+
+  // 新增完後印出 cookie
+  // ses.cookies.set(cookie, err => {
+  //   console.log('cookie1 set')
+  //   getCookies()
+  // })
+
+  // 等頁面載入完後抓取頁面 cookies
+  // mainWindow.webContents.on('did-finish-load', e => {
+  //   getCookies()
+  // })
 
   // Open DevTools - Remove for PRODUCTION!
   mainWindow.webContents.openDevTools()
