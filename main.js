513f48900448f1a6ad54cb0969376e1a4114fca4
hsimao <e087754958@gmail.com>
2019-12-28 15:25:29 +0800

    add: handle  download api

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Modules
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, session } = require('electron')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -7,11 +7,12 @@ let mainWindow
 
 // Create a new BrowserWindow when `app` is ready
 function createWindow() {
+  let ses = session.defaultSession
+
   mainWindow = new BrowserWindow({
     width: 1000,
     height: 800,
     webPreferences: { nodeIntegration: true },
-    backgroundColor: '#2c92f9',
   })
 
   // Load index.html into the new BrowserWindow
@@ -19,6 +20,28 @@ function createWindow() {
 
   // Open DevTools - Remove for PRODUCTION!
   mainWindow.webContents.openDevTools()
+
+  // 監聽下載事件
+  ses.on('will-download', (e, downloadItem, webContents) => {
+    const fileName = downloadItem.getFilename()
+    let fileSize = downloadItem.getTotalBytes()
+
+    // 將檔案下載到桌面
+    downloadItem.setSavePath(app.getPath('desktop') + `/${fileName}`)
+
+    downloadItem.on('updated', (e, state) => {
+      // 已下載大小
+      let received = downloadItem.getReceivedBytes()
+      console.log('received', received)
+
+      if (state === 'progressing' && received) {
+        // 計算下載進度
+        let progress = Math.round((received / fileSize) * 100)
+        console.log('progress', progress)
+        webContents.executeJavaScript(`window.progress.value = ${progress}`)
+      }
+    })
+  })
 
   // Listen for window being closed
   mainWindow.on('closed', () => {
