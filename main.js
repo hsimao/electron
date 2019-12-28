f895e322eea12afd0a328931501d3cb3d946f6f2
hsimao <e087754958@gmail.com>
2019-12-28 16:19:02 +0800

    add: dialog 彈窗

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Modules
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, dialog } = require('electron')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -19,6 +19,46 @@ function createWindow() {
 
   // Open DevTools - Remove for PRODUCTION!
   mainWindow.webContents.openDevTools()
+
+  // 當內容載入完畢後開啟選擇檔案視窗
+  mainWindow.webContents.on('did-finish-load', () => {
+    // 開啟選擇檔案視窗
+    // dialog.showOpenDialog(
+    //   mainWindow,
+    //   {
+    //     buttonLabel: '選擇檔案',
+    //     // 開啟選擇檔案視窗的預設目標路徑, 以下為桌面
+    //     defaultPath: app.getPath('desktop'),
+    //     // 選擇設置
+    //     // mutiSelections: 允許允許選擇多個路徑
+    //     // createDirectory: 允許從對話框創建新目錄
+    //     // openFile: 允許選擇文件
+    //     // openDirectory: 允許選擇目錄
+    //     properties: ['multiSelections', 'createDirectory', 'openFile', 'openDirectory'],
+    //   },
+    //   filepaths => {
+    //     console.log(filepaths)
+    //   }
+    // )
+
+    // dialog.showSaveDialog({}, filename => {
+    //   console.log('filename', filename)
+    // })
+
+    // 文字選擇彈窗
+    const answers = ['Yes', 'No', 'Maybe']
+    dialog.showMessageBox(
+      {
+        title: 'Message Box',
+        message: 'Please select an option',
+        detail: 'Message details.',
+        buttons: answers,
+      },
+      res => {
+        console.log(`你選擇了: ${answers[res]}`)
+      }
+    )
+  })
 
   // Listen for window being closed
   mainWindow.on('closed', () => {
