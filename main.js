be06a14052a66b377dd820ee434a173d0743f90a
hsimao <e087754958@gmail.com>
2019-12-31 20:28:29 +0800

    add: offscreen

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 // Modules
 const { app, BrowserWindow } = require('electron')
+const fs = require('fs')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -10,20 +11,41 @@ function createWindow() {
   mainWindow = new BrowserWindow({
     width: 1000,
     height: 800,
-    webPreferences: { nodeIntegration: true },
+    show: false,
+    webPreferences: {
+      nodeIntegration: true,
+      // 開啟 offscreen 模式
+      offscreen: true,
+    },
     backgroundColor: '#2c92f9',
   })
 
   // Load index.html into the new BrowserWindow
-  mainWindow.loadFile('index.html')
+  // mainWindow.loadFile('index.html')
+  mainWindow.loadURL('https://google.com')
 
-  // Open DevTools - Remove for PRODUCTION!
-  mainWindow.webContents.openDevTools()
+  let i = 1
+  // 印出載入 google.com 網頁時的每偵畫面
+  mainWindow.webContents.on('paint', (e, dirty, image) => {
+    let screenshot = image.toPNG()
+    fs.writeFile(app.getPath('desktop') + `/screenshot_${i}.png`, screenshot, console.log)
+    i++
+  })
 
-  // Listen for window being closed
-  mainWindow.on('closed', () => {
+  mainWindow.webContents.on('did-finish-load', e => {
+    console.log(mainWindow.getTitle())
+
+    mainWindow.close()
     mainWindow = null
   })
+
+  // Open DevTools - Remove for PRODUCTION!
+  // mainWindow.webContents.openDevTools()
+
+  // Listen for window being closed
+  // mainWindow.on('closed', () => {
+  //   mainWindow = null
+  // })
 }
 
 // Electron `app` is ready
